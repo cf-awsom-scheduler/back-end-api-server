1087374788c0aa0a2c9f6e8e4b35f2138d4dc0f1
Morgana Spake <morgana.spake@gmail.com>
2019-07-16 15:44:43 -0700

    model changes

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
--- Models/TrialRequestModel.cs
+++ Models/TrialRequestModel.cs
@@ -9,40 +9,27 @@ namespace awsomAPI.Models {
   public class TrialRequest {
     [Key, Required]
     public long Id { get; set; }
-
     [Required]
     public string ParentName {get; set;}
-
     [Required]
     public string StudentName {get; set;}
-
     public string StudentBirthDate {get; set;}
-
     [Required]
     public string Email {get; set;}
-
     public string Phone {get; set;}
-
     [Required]
     public string Address {get; set;}
-
     [Required]
     public string City {get; set;}
-
     [Required]
-    public int ZipCode {get; set;}
-
+    public string ZipCode {get; set;}
     [Required]
     public string Region {get; set;}
-
     [Required]
-    public bool HasInstrument {get; set;}
-
+    public string Instrument {get; set;}
+    public string HasInstrument {get; set;}
     public string Availability {get; set;}
-
     public string Experience {get; set;}
-
     public string Notes {get; set;}
-
   }
 }
