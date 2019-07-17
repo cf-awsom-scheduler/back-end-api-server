b32e4933531725349a18af177ba23933d2951e6f
Morgana Spake <morgana.spake@gmail.com>
2019-07-17 11:43:15 -0700

    switched back to production string

--- Startup.cs
+++ Startup.cs
@@ -41,7 +41,7 @@ namespace awsomAPI
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<AwsomApiContext>( opt => 
-              opt.UseSqlServer(Configuration.GetConnectionString("DefaultString")));
+              opt.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
 
             services.AddAuthentication( auth => 
             {
