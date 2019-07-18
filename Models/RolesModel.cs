b284991f128dd56164dcbdca610015c313f1b511
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-07-18 10:56:21 -0700

    RoleModel: doc comments

--- Models/RolesModel.cs
+++ Models/RolesModel.cs
@@ -1,10 +1,29 @@
+///-------------------------------------------------------------------------------------------------
+// file:	Models\RolesModel.cs
+//
+// summary:	Implements the roles model class
+///-------------------------------------------------------------------------------------------------
+
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
+///-------------------------------------------------------------------------------------------------
+// namespace: awsomAPI.Models
+//
+// summary:	.
+///-------------------------------------------------------------------------------------------------
+
 namespace awsomAPI.Models
 {
+    ///-------------------------------------------------------------------------------------------------
+    /// <summary>   A role model. </summary>
+    ///
+    /// <remarks>   Vanvoljg, 18-Jul-19. </remarks>
+    ///-------------------------------------------------------------------------------------------------
+
     public static class Role
     {
         public const string Admin = "admin";
