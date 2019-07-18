d7d8f2d584a883408b187b12678a055e239a4ce4
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-07-18 10:57:18 -0700

    SelectedStudents: doc comments

--- Models/SelectedStudents.cs
+++ Models/SelectedStudents.cs
@@ -1,15 +1,54 @@
+///-------------------------------------------------------------------------------------------------
+// file:	Models\SelectedStudents.cs
+//
+// summary:	Implements the selected students class
+///-------------------------------------------------------------------------------------------------
+
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
+///-------------------------------------------------------------------------------------------------
+// namespace: awsomAPI.Models
+//
+// summary:	.
+///-------------------------------------------------------------------------------------------------
+
 namespace awsomAPI.Models
 {
+    ///-------------------------------------------------------------------------------------------------
+    /// <summary>   A student teacher selected relation. </summary>
+    ///
+    /// <remarks>   Vanvoljg, 18-Jul-19. </remarks>
+    ///-------------------------------------------------------------------------------------------------
+
     public class StudentTeacherSelectedRelation
     {
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets or sets the identifier. </summary>
+        ///
+        /// <value> The identifier. </value>
+        ///-------------------------------------------------------------------------------------------------
+
         [Key, Required]
         public long Id { get; set; }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets or sets the identifier of the trial request. </summary>
+        ///
+        /// <value> The identifier of the trial request. </value>
+        ///-------------------------------------------------------------------------------------------------
+
         public string TrialRequestId { get; set; }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets or sets the identifier of the teacher. </summary>
+        ///
+        /// <value> The identifier of the teacher. </value>
+        ///-------------------------------------------------------------------------------------------------
+
         public string TeacherId { get; set; }
     }
 }
