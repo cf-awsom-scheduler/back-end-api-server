82f436902fc099fe2b881ce71b5890b0ba2220c7
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-07-18 10:36:21 -0700

    Program, Startup: formatting & doc comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿///-------------------------------------------------------------------------------------------------
+// file:	Program.cs
+//
+// summary:	Implements the program class
+///-------------------------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,14 +14,39 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+
+///-------------------------------------------------------------------------------------------------
+// namespace: awsomAPI
+//
+// summary:	.
+///-------------------------------------------------------------------------------------------------
+
 namespace awsomAPI
 {
     public class Program
     {
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Main entry-point for this application. </summary>
+        ///
+        /// <remarks>   Vanvoljg, 18-Jul-19. </remarks>
+        ///
+        /// <param name="args"> An array of command-line argument strings. </param>
+        ///-------------------------------------------------------------------------------------------------
+
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Creates web host builder. </summary>
+        ///
+        /// <remarks>   Vanvoljg, 18-Jul-19. </remarks>
+        ///
+        /// <param name="args"> An array of command-line argument strings. </param>
+        ///
+        /// <returns>   The new web host builder. </returns>
+        ///-------------------------------------------------------------------------------------------------
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿///-------------------------------------------------------------------------------------------------
+// file:	Startup.cs
+//
+// summary:	Implements the startup class.
+///-------------------------------------------------------------------------------------------------
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -17,21 +23,45 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using awsomAPI.Models;
 
-//Morgana - JWT Auth Functionality found here https://jasonwatmore.com/post/2019/01/08/aspnet-core-22-role-based-authorization-tutorial-with-example-api#app-settings-json
+//Morgana - JWT Auth Functionality found here https://jasonwatmore.com/post/2019/01/08/aspnet-core-22-role-based-authorization-tutorial-with-example-api#app-settings-json.
+
+
+///-------------------------------------------------------------------------------------------------
+// namespace: awsomAPI
+//
+// summary:	.
+///-------------------------------------------------------------------------------------------------
 
 namespace awsomAPI
 {
     public class Startup
     {
-      private string _secret = null;
+        private string _secret = null;
+
+        public IConfiguration Configuration { get; }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Constructor. </summary>
+        ///
+        /// <remarks>   Vanvoljg, 18-Jul-19. </remarks>
+        ///
+        /// <param name="configuration">    The configuration. </param>
+        ///-------------------------------------------------------------------------------------------------
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Configure services. </summary>
+        ///
+        /// <remarks>   Vanvoljg, 18-Jul-19. </remarks>
+        ///
+        /// <param name="services"> The services. </param>
+        ///-------------------------------------------------------------------------------------------------
         // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
         {
             _secret = Configuration["Secret"];
@@ -40,29 +70,37 @@ namespace awsomAPI
             services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddDbContext<AwsomApiContext>( opt => 
-              opt.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
+            services.AddDbContext<AwsomApiContext>(opt =>
+            opt.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
 
-            services.AddAuthentication( auth => 
+            services.AddAuthentication(auth =>
             {
-              auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-              auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer( jwt => 
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(jwt =>
             {
-              jwt.RequireHttpsMetadata = true;
-              jwt.SaveToken = true;
-              jwt.TokenValidationParameters = new TokenValidationParameters
-              {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(_key),
-                ValidateIssuer = false,
-                ValidateAudience = false
-              };
+                jwt.RequireHttpsMetadata = true;
+                jwt.SaveToken = true;
+                jwt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(_key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
             });
         }
 
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Configures. </summary>
+        ///
+        /// <remarks>   Vanvoljg, 18-Jul-19. </remarks>
+        ///
+        /// <param name="app">  The application. </param>
+        /// <param name="env">  The environment. </param>
+        ///-------------------------------------------------------------------------------------------------
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -78,7 +116,7 @@ namespace awsomAPI
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseMvc();
-            app.UseCors( settings => settings.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseCors(settings => settings.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
         }
     }
 }
